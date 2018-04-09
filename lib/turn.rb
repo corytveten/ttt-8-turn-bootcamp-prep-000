3a79b3de1f0ad1118c3d4ba0ec0b8a0605683685
corytveten <corytveten@gmail.com>
2018-04-09 20:03:38 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -10,3 +10,24 @@ def display_board(board)
 end
 
 display_board(board)
+
+def valid_move?(board, index)
+  if index >= 0 && index < 9
+    if position_taken?(board, index) == false
+      return true
+    elsif position_taken?(board, index) == true
+      return false
+    end
+  else
+    return false
+end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  else
+    return true
+  end
+end
